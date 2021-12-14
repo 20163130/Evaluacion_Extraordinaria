714f05107ab1fd3af30d6ea8c92ea98640fe00b9
Nora <2016313057@uteq.edu.mx>
2021-12-13 20:52:24 -0600

    Create ExtraordinarioNora.py

---
+++ ExtraordinarioNora.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+modelo_multiple <- lm(formula = medv ~ ., data = Boston)
+
+
+# In[2]:
+
+
+summary(modelo_multiple)
+
+
+# In[3]:
+
+
+names(modelo_simple)
+
+
+# In[4]:
+
+
+import numpy as np
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+
+
